8a757f897f34e9a7cbcc846fb37a33dac921faa6
Ram <ramanji2585.com>
2021-12-05 20:53:07 +0530

    WAR FILE

---
+++ src/main/java/com/techempower/Server.java
@@ -0,0 +1,25 @@
+package com.techempower;
+
+import com.techempower.gemini.transport.*;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.annotation.WebServlet;
+
+/**
+ * Interface to the application. 
+ */
+@SuppressWarnings("serial")
+@MultipartConfig(fileSizeThreshold = 1024 * 1024 * 20, maxRequestSize = 1024 * 1024 * 50)
+@WebServlet(name="SRV", urlPatterns="*")
+public class Server
+     extends InfrastructureServlet
+{
+    //
+    // Member methods
+    //
+
+    @Override
+    public Application getApplication() {
+        return Application.getInstance();
+    }
+    
+}
---
+++ src/main/java/com/techempower/handlers/HomeHandler.java
@@ -0,0 +1,20 @@
+package com.techempower.handlers;
+
+import com.techempower.gemini.*;
+import com.techempower.gemini.path.*;
+import com.techempower.gemini.path.annotation.*;
+
+public class HomeHandler extends MethodUriHandler<Context>
+{
+
+  public HomeHandler(GeminiApplication app)
+  {
+    super(app);
+  }
+
+  @Path
+  @Get
+  public boolean root() throws Exception {
+    return json();
+  }
+}
